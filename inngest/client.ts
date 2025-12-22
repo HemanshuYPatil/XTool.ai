5cd443bbf0547375b0560fdf5673a93ba91ecc00
HemanshuYPatil <hemanshuypatil@gmail.com>
2025-12-22 20:06:18 +0530

    Event Key Issue

--- inngest/client.ts
+++ inngest/client.ts
@@ -3,6 +3,7 @@ import { realtimeMiddleware } from "@inngest/realtime/middleware";
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "xdesign-app",
+  eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY,
   middleware: [realtimeMiddleware()],
 });
